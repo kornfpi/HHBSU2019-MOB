e55946c2fdb4521c353471813e31b91abdec83a2
John W <wolford.72@osu.edu>
2019-07-16 10:34:46 -0400

    Update Log.cs

--- HabitatBuddy/HabitatBuddy/Models/Log.cs
+++ HabitatBuddy/HabitatBuddy/Models/Log.cs
@@ -9,6 +9,7 @@ namespace HabitatBuddy.Models
 
     public class Log
     {
+        public int Number { get; set; }
         public string HomeNumber { get; set; }
         public string HomeOwner { get; set; }
         public string Action { get; set; }
